6f09fdf4a7f8bf4c0d39b3898ca69939866f0226
fhurtado <fhurtado@fhurtado-Dell-System-Inspiron-N4110>
2015-06-02 17:48:03 -0500

    Agrego la libreria de Sencha 3.0.1 para crear a graficas estadisticas

---
+++ src/java/co/com/sisegfut/client/administracion/Competencia/PanelEstadisticas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.sisegfut.client.administracion.Competencia;
+
+import com.sencha.gxt.widget.core.client.FramedPanel;
+
+/**
+ *
+ * @author fhurtado
+ */
+public class PanelEstadisticas {
+    
+    private FramedPanel panel;
+    
+    
+    
+    
+    
+    
+    
+}
