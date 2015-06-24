8163119cb069c64f1847c10100313c9bb448048d
Andres Hurtado <Andres Hurtado@Andresh>
2015-06-23 19:45:30 -0500

    se agrega ejemplo de grafica de barras sencha 3.0

--- src/java/co/com/sisegfut/client/administracion/Competencia/PanelEstadisticas.java
+++ src/java/co/com/sisegfut/client/administracion/Competencia/PanelEstadisticas.java
@@ -6,6 +6,7 @@
 package co.com.sisegfut.client.administracion.Competencia;
 
 import co.com.sisegfut.client.aatest.AdvancedChartExample;
+import co.com.sisegfut.client.aatest.ejemploBarras;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.IconButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
@@ -13,6 +14,7 @@ import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.button.ToolButton;
 import com.extjs.gxt.ui.client.widget.layout.FillLayout;
+import com.extjs.gxt.ui.client.widget.layout.MarginData;
 import com.sencha.gxt.widget.core.client.FramedPanel;
 
 /**
@@ -21,8 +23,9 @@ import com.sencha.gxt.widget.core.client.FramedPanel;
  */
 public class PanelEstadisticas extends Window {
 
-    private FramedPanel panel;
+    private FramedPanel panel = new FramedPanel();
     private AdvancedChartExample advancedChartExample;
+//    private ejemploBarras ejemploBarras1 = new ejemploBarras();
     private Long idCompetencia;
 
     public PanelEstadisticas() {
@@ -34,8 +37,15 @@ public class PanelEstadisticas extends Window {
         setBlinkModal(true);
         setHeading("Estadistica de la competencia");
         setLayout(new FillLayout());
+        
+        panel.setLayoutData(new MarginData(10));
+        panel.setCollapsible(true);
+        panel.setHeadingText("Grouped Bar Chart");
+        panel.setPixelSize(620, 500);
+        panel.setBodyBorder(true);
 
-        add(advancedChartExample);
+//        panel.add(ejemploBarras1);
+        add(panel);
         Button btnCancelarBusCompetencia = new Button("Cancelar", new SelectionListener<ButtonEvent>() {
 
             @Override
@@ -57,6 +67,5 @@ public class PanelEstadisticas extends Window {
     public void setIdCompetencia(Long idCompetencia) {
         this.idCompetencia = idCompetencia;
     }
-    
 
 }
